b55dbc3973baa77cb87a0890ab698850d55662c8
A M Akankshit <akankshitmishra@gmail.com>
2019-02-21 22:52:37 +0530

    success and failure format added

--- backend/node/index.js
+++ backend/node/index.js
@@ -1,7 +1,10 @@
 const config = require('./config/config.json');
+
 const express = require('express');
 const app = express();
-
 var port = process.env.PORT || config.node_port;
-
 app.listen(port);
+
+app.get('/',(req, res) => {
+    
+})
